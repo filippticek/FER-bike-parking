ecf0ce36760f1333efac658b39b5fc21459597ac
shiranaii <barbara.klier@gmail.com>
2019-05-12 19:36:41 +0200

    Updated

--- html/readerHandler.php
+++ html/readerHandler.php
@@ -11,12 +11,13 @@ echo $input->reader;
 echo "\n";
 echo $input->id;
 echo "\n";
+//var_dump(http_response_code(200));
 
 
 
 // get the HTTP method, path and body of the request
 if (isset ($_SERVER["REQUEST_METHOD"])){
-  echo "set";
+  echo "\nset";
 }
 else {
   echo "not set";
@@ -25,9 +26,20 @@ else {
 $method = $_SERVER["REQUEST_METHOD"];
 $value = getenv('REQUEST_METHOD');
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/')); //url
+//$input = json_decode(file_get_contents('php://input'),true);
+
+
+// die if SQL statement failed
+if ($result==1) {
+  http_response_code(200);
+  //die(mysqli_error());
+}
+else {
+    http_response_code(401);
+}
 
 http_response_code(401);
+echo "response code: "
 var_dump(http_response_code());
-// print results, insert id or affected row count
 
 ?>

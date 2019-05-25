5d7462c995f150ae400650830ab1e9ffb9613e57
shiranaii <barbara.klier@gmail.com>
2019-05-25 12:21:30 +0200

    Fixed bug

--- html/readerHandler.php
+++ html/readerHandler.php
@@ -5,73 +5,46 @@ $json = '{
     "id": "E20034120138F20010D5CFAF"
 }';
 $json = file_get_contents('php://input');
-
 $input = json_decode($json);
 
 echo $input->reader;
 echo  nl2br (" \n ");
 echo $input->id;
 echo  nl2br (" \n ");
-//var_dump(http_response_code(200));
 
-
-
-// get the HTTP method, path and body of the request
-//if (isset ($_SERVER["REQUEST_METHOD"])){
-//  echo "set";
-//}
-//else {
-//  echo "not set";
-//}
-
+// read request
 $method = $_SERVER["REQUEST_METHOD"];
 $value = getenv('REQUEST_METHOD');
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/')); //url
-//$input = json_decode(file_get_contents('php://input'),true);
 
 // connect to the mysql database
 $link = mysqli_connect('localhost', 'root', 'password', 'bikeParking');
 mysqli_set_charset($link,'utf8');
 
-// retrieve the table and key from the path
-//$table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
-//$key = array_shift($request)+0;
-
-
-// create SQL based on HTTP method
-
-    $sql = "SELECT * FROM tags WHERE id1='$input->id'";
-    $result = mysqli_query($link,$sql);
-
-    $row = mysqli_fetch_assoc($result);
+// create SQL query
+$sql = "SELECT * FROM tags WHERE id1='$input->id'";
+$result = mysqli_query($link,$sql);
+$row = mysqli_fetch_assoc($result);
 
 echo "rezultat: ";
-//var_dump($result);
 echo mysqli_num_rows($result);
 //echo  nl2br (" \n ");
 
-
-// die if SQL statement failed
+// set resonse codes
 if (mysqli_num_rows($result)==1) {
   http_response_code(200);
-  //die(mysqli_error());
 }
 else {
   http_response_code(401);
 }
 
-//echo "response:";
-//var_dump(http_response_code());
 
 // print results, insert id or affected row count
 if ($method == 'POST') {
 
-  if (!$key) echo '[';
   for ($i=0;$i<mysqli_num_rows($result);$i++) {
     echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
   }
 
-  if (!$key) echo ']';
 } elseif ($method == 'POST') {
   echo mysqli_insert_id($link);
 
@@ -82,5 +55,5 @@ if ($method == 'POST') {
 
 // close mysql connection
 mysqli_close($link);
-require "footer.php"; 
+require "footer.php";
 ?>

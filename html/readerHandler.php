c392bc3c9db5631b104cd29f406e33c07a9ca2b9
shiranaii <barbara.klier@gmail.com>
2019-05-25 12:50:40 +0200

    Beautified code

--- html/readerHandler.php
+++ html/readerHandler.php
@@ -1,5 +1,6 @@
 <?php require "header.php";
 
+//default data and set data
 $json = '{
     "reader": "nfc",
     "id": "E20034120138F20010D5CFAF"
@@ -14,46 +15,34 @@ echo  nl2br (" \n ");
 
 // read request
 $method = $_SERVER["REQUEST_METHOD"];
-$value = getenv('REQUEST_METHOD');
 
-// connect to the mysql database
-$link = mysqli_connect('localhost', 'root', 'password', 'bikeParking');
-mysqli_set_charset($link,'utf8');
+if($method == 'POST'){
 
-// create SQL query
-$sql = "SELECT * FROM tags WHERE id1='$input->id'";
-$result = mysqli_query($link,$sql);
-$row = mysqli_fetch_assoc($result);
+  // connect to the mysql database
+  $link = mysqli_connect('localhost', 'root', 'password', 'bikeParking');
+  mysqli_set_charset($link,'utf8');
 
-echo "rezultat: ";
-echo mysqli_num_rows($result);
-//echo  nl2br (" \n ");
+  // create SQL query
+  $sql = "SELECT * FROM tags WHERE id1='$input->id'";
+  $result = mysqli_query($link,$sql);
+  $row = mysqli_fetch_assoc($result);
 
-// set resonse codes
-if (mysqli_num_rows($result)==1) {
-  http_response_code(200);
-}
-else {
-  http_response_code(401);
-}
+  //print results
+  echo "rezultat: ";
+  echo mysqli_num_rows($result);
+  echo  nl2br (" \n ");
 
-
-// print results, insert id or affected row count
-if ($method == 'POST') {
-
-  for ($i=0;$i<mysqli_num_rows($result);$i++) {
-    echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
+  // set resonse codes
+  if (mysqli_num_rows($result)==1) {
+    http_response_code(200);
+  }
+  else {
+    http_response_code(401);
   }
 
-} elseif ($method == 'POST') {
-  echo mysqli_insert_id($link);
-
-} else {
-  echo "rows: " . mysqli_affected_rows($link);
+  // close mysql connection
+  mysqli_close($link);
 }
 
-
-// close mysql connection
-mysqli_close($link);
 require "footer.php";
 ?>

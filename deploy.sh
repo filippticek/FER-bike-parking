789bc4c6d6166e9aed726aca7e99b4fda113c8c2
sebojanko <janko.sebastian@gmail.com>
2019-05-29 19:57:48 +0200

    Added start on boot

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,7 @@ mkdir venv
 virtualenv venv -p /usr/bin/python3
 source venv/bin/activate
 pip install -r requirements.txt
+pip install supervisor
 
 
 # setup network interface
@@ -26,7 +27,14 @@ chmod +x network_setup.sh
 ./network_setup.sh
 
 # setup startup script?
+crontab -l > mycron
+echo "@reboot /home/pi/FER-bike-parking/venv/bin/supervisord" > mycron
+crontab mycron
+rm mycron
 
 # setup supervisor daemon to automatically restart apps in case of crashing
 sudo cp supervisord.conf /etc/supervisord.conf
-./supervisord
+pkill -f supervisord
+venv/bin/supervisord
+
+echo "Deployed successfully!"

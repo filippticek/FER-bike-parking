facaf8cb97c3c52f85ef7ebbb76a765f5c61e0d4
Filip <filip.pticek@fer.hr>
2021-11-22 13:19:22 +0100

    Rewrite deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,41 +1,71 @@
 #!/bin/bash
 
-cd ~
-cd FER-bike-parking/
+while getopts 'aur' OPTION; do
+    case "$OPTION" in
+        a)
+			pull_changes
+			install_dependecies
+			nfc_uhf_setup
+			restart_services
+            ;;
+        u)
+			pull_changes
+			install_dependecies
+			uhf_setup
+			restart_services
+            ;;
+        r)
+			pull_changes
+			restart_services
+            ;;
+        *)
+            echo "Usage: $0 [-a] [-u] [-r]" >&2
+			echo "Supply the following arguments:"
+			echo "      -a for deploying nfc/uhf box"
+			echo "      -u for deploying uhf box"
+			echo "      -r for restarting the service"
+            exit 1
+			;;
+    esac
+done
 
-# clone repo
-git pull https://github.com/filippticek/FER-bike-parking.git
-status=$?
-if [[ $status -eq 128 ]];
-then
-	git clone https://github.com/filippticek/FER-bike-parking.git
-fi
+pull_changes () {
+	cd ~/FER-bike-parking/
+	# pull latest changes
+	git pull 
+}
 
-cd FER-bike-parking/
-git branch test
+install_dependecies (){
+	sudo apt install -y supervisor
+	# setup virtual env
+	sudo pip3 install virtualenv
+	mkdir venv
+	virtualenv venv -p /usr/bin/python3
+	source venv/bin/activate
+	pip install -r requirements.txt
+}
 
-# setup virtual env
-sudo pip3 install virtualenv
-mkdir venv
-virtualenv venv -p /usr/bin/python3
-source venv/bin/activate
-pip install -r requirements.txt
-pip install supervisor
+uhf_setup () {
+	# add supervisord configuration 
+	sudo cp supervisord_uhf.conf /etc/supervisor/supervisord.conf
+}
 
+nfc_uhf_setup () {
+	# setup network interface
+	chmod +x network_setup.sh
+	./network_setup.sh
 
-# setup network interface
-chmod +x network_setup.sh
-./network_setup.sh
+	# setup startup script
+	crontab -l > mycron
+	echo "@reboot /home/pi/FER-bike-parking/./network_setup.sh" > mycron
+	crontab mycron
+	rm mycron
 
-# setup startup script
-crontab -l > mycron
-echo "@reboot /home/pi/FER-bike-parking/./network_setup.sh && /home/pi/FER-bike-parking/venv/bin/supervisord" > mycron
-crontab mycron
-rm mycron
+	# add supervisord configuration 
+	sudo cp supervisord.conf /etc/supervisor/supervisord.conf
+}
 
-# setup supervisor daemon to automatically restart apps in case of crashing
-sudo cp supervisord.conf /etc/supervisord.conf
-pkill -f supervisord
-venv/bin/supervisord
-
-echo "Deployed successfully!"
+restart_services() {
+	# restart supervisor
+	sudo service supervisor restart
+}

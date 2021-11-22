11c8715a99fb56cd562ec040cc9578db13f2edfb
Filip <filip.pticek@fer.hr>
2021-11-22 13:47:02 +0100

    Add program flow text print

--- deploy.sh
+++ deploy.sh
@@ -3,41 +3,50 @@
 pull_changes () {
 	cd ~/FER-bike-parking/
 	# pull latest changes
+	echo "Pulling latest changes"
 	git pull 
 }
 
 install_dependecies () {
+	echo "Installing supervisor"
 	sudo apt install -y supervisor
 	# setup virtual env
+	echo "Setting up virtualenv"
 	sudo pip3 install virtualenv
 	mkdir venv
 	virtualenv venv -p /usr/bin/python3
 	source venv/bin/activate
+	echo "Installing requirements"
 	pip install -r requirements.txt
 }
 
 uhf_setup () {
 	# add supervisord configuration 
+	echo "Adding supervisord configuration"
 	sudo cp supervisord_uhf.conf /etc/supervisor/supervisord.conf
 }
 
 nfc_uhf_setup () {
 	# setup network interface
+	echo "Adding subnet"
 	chmod +x network_setup.sh
 	./network_setup.sh
 
 	# setup startup script
+	echo "Setting up cronjob"
 	crontab -l > mycron
 	echo "@reboot /home/pi/FER-bike-parking/./network_setup.sh" > mycron
 	crontab mycron
 	rm mycron
 
 	# add supervisord configuration 
+	echo "Adding supervisord configuration"
 	sudo cp supervisord.conf /etc/supervisor/supervisord.conf
 }
 
 restart_services () {
 	# restart supervisor
+	echo "Restarting supervisor"
 	sudo service supervisor restart
 }
 
